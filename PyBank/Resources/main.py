420f1ed479d2549456efc288e69275ee4854b09f
robertoj-coder <robertoj.delatorre@gmail.com>
2020-08-19 20:49:21 -0400

    first part of the analysis

---
+++ PyBank/Resources/main.py
@@ -0,0 +1,37 @@
+import os
+import csv
+
+
+budget_path = os.path.join("budget_data.csv")
+
+with open(budget_path, "r") as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+    # Reset value to 0 to calculate total profit(totalP)and number of months
+    totalP = 0
+    months = 0
+
+    header = next(csvreader)
+
+    for row in csv.reader(csvfile):
+
+        totalP = totalP + (int(row[1]))
+        months = months + 1
+
+print("Total months: " + str(months))
+print("Total volume: " + str(totalP))
+
+
+
+
+        
+
+
+ 
+
+
+
+
+
+
